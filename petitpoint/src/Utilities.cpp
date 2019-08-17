8dc5ccc5968252c8df136c88b97905ee22e96c9b
gdenhez <gdenhez42@gmail.com>
2019-08-17 15:24:04 -0400

    Fix collisions

--- petitpoint/src/Utilities.cpp
+++ petitpoint/src/Utilities.cpp
@@ -5,10 +5,10 @@ namespace pp {
 
 	bool AreColliding(const Rectangle& p_Rec1, const Rectangle& p_Rec2)
 	{
-		return !(p_Rec1.m_x + p_Rec1.m_w < p_Rec2.m_x ||
-			p_Rec2.m_x + p_Rec2.m_w < p_Rec1.m_x) &&
-			!(p_Rec1.m_y + p_Rec1.m_h < p_Rec2.m_y ||
-				p_Rec2.m_y + p_Rec2.m_h < p_Rec1.m_y);
+		return !(p_Rec1.m_x + p_Rec1.m_w <= p_Rec2.m_x ||
+			p_Rec2.m_x + p_Rec2.m_w <= p_Rec1.m_x) &&
+			!(p_Rec1.m_y + p_Rec1.m_h <= p_Rec2.m_y ||
+				p_Rec2.m_y + p_Rec2.m_h <= p_Rec1.m_y);
 	}
 
     void Log(const std::string& p_string)
